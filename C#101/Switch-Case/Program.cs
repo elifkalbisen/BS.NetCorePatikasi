7671e45793613a2b77f785ac5ac39c06a0867f40
elifkalbisen <eliffdoruk@gmail.com>
2025-05-16 14:40:25 +0300

    ekleme
    dosyalar eklendi.

---
+++ C#101/Switch-Case/Program.cs
@@ -0,0 +1,55 @@
+﻿int month = DateTime.Now.Month;
+
+switch (month) //kontrol ettiğimiz koşulu
+{
+    case 1:
+        Console.WriteLine("Ocak");
+        break;
+    case 2:
+        Console.WriteLine("Şubat");
+        break;
+    case 4:
+        Console.WriteLine("Nisan");
+        break;
+    case 3:
+        Console.WriteLine("Mart");
+        break;
+    case 5:
+        Console.WriteLine("Mayıs");
+        break;
+    case 6:
+        Console.WriteLine("Haziran");
+        break;
+
+    default:
+        Console.WriteLine("Yanlış veri girdiniz");
+        break;
+}
+
+
+switch (month)
+{
+    case 12:
+    case 1:
+    case 2:
+        Console.WriteLine("Kış");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("İlkbahar");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        Console.WriteLine("Yaz");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        Console.WriteLine("Sonbahar");
+        break;
+    default:
+        Console.WriteLine("Yanlış veri girdiniz");
+    break;
+}
