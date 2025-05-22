b2dceaf2d41a9f2f384b95134eebb661c0a0ea21
elifkalbisen <eliffdoruk@gmail.com>
2025-05-22 21:34:42 +0300

    ekleme
    ekleme

---
+++ C#101/Odev2/Koleksiyonlar-Soru-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+ArrayList asalListesi = new ArrayList();
+ArrayList asalOlmayanListesi = new ArrayList();
+Console.WriteLine("Pozitif bir tam sayı giriniz:");
+for (int i = 0; i < 20; i++)
+{
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    if (AsalMi(sayi))
+        asalListesi.Add(sayi);
+    else
+        asalOlmayanListesi.Add(sayi);
+}
+Console.WriteLine("Asal sayılar:");
+foreach (int item in asalListesi)
+    Console.WriteLine(item);
+
+Console.WriteLine("Asal olmayan sayılar:");
+foreach (int item in asalOlmayanListesi)
+    Console.WriteLine(item);
+
+static bool AsalMi(int sayi)
+{
+    if (sayi < 2)
+        return false;
+    for (int i = 2; i <= Math.Sqrt(sayi); i++)
+    {
+        if (sayi % i == 0)
+            return false;
+    }
+    return true;
+}
